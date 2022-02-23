244f8bc76eb41e14cdb55534b741fc70130d4fe2
Hasibur Rahman <hasib.active@gmail.com>
2022-02-24 01:30:00 +0600

    init app

---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,18 @@
+<template>
+   <div>
+      <the-header></the-header>
+      <h1>About Page</h1>
+   </div>
+</template>
+
+<script>
+import TheHeader from '../components/TheHeader.vue'
+export default {
+   components: {
+      TheHeader,
+   },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ resources/js/components/TheHeader.vue
@@ -0,0 +1,22 @@
+<template>
+   <div>
+      <ul>
+         <li>
+            <Link href="/">Home</Link>
+            <Link href="/about">About</Link>
+         </li>
+      </ul>
+   </div>
+</template>
+
+<script>
+import { Link } from '@inertiajs/inertia-vue3'
+export default {
+   components: {
+      Link,
+   },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
